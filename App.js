50a0e9f8f796c4ee1ef8f64d750841c1ec050dc7
NadaSoliman603 <nada.educate@gmail.com>
2020-10-13 09:27:08 -0700

    Navigate Betwen pages

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
     list: listScreen
   },
   {
-    initialRouteName: "list",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
       title: "App"
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,39 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button ,TouchableOpacity} from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = (props) => {
+  return (
+    console.log(props),
+    <View>
+      <Text style={styles.text } >HomeScreen</Text>
+      <Button
+        onPress={() =>props.navigation.navigate('list') }
+        title="Go To List Screen"
+        
+       />
+       <Text style={styles.buttonStyl}></Text>
+       <Button
+       onPress={()=> props.navigation.navigate("component")}
+        title="Go to componentScreen"
+       />
+
+       <TouchableOpacity style={styles.text} /*onPress={() =>props.navigation.navigate('list')}*/ >
+         <Text>hello woeld</Text>
+         <Text>hello woeld</Text>
+         <Text>hello woeld</Text>
+         <Text>hello woeld</Text>
+       </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+    marginVertical: 30
+  },
+  buttonStyl: {
+    marginVertical: 10
   }
 });
 
